1e8c5fb8a839ea5f1686a29b6bffbd05f92840dd
dell19 <dell19@dell19.dell>
2015-10-31 13:54:54 +0100

    added flasher and geany config

---
+++ conf_geany/filedefs/filetypes.c
@@ -0,0 +1,10 @@
+[build-menu]
+FT_00_LB=_Build
+FT_00_CM=make all
+FT_00_WD=
+FT_01_LB=Flash
+FT_01_CM=make all && mv main.hex flash.hex && make clean && ../../con_flash /dev/ttyUSB0 flash.hex
+FT_01_WD=
+FT_02_LB=_Clean
+FT_02_CM=make clean
+FT_02_WD=
