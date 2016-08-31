bae593e6e0c06e21b68d3a072976125c69e6bf0e
1 <1>
2016-08-31 12:49:44 +0000

    fabi and sally

--- projects/Fabi, Salli/helpers.h
+++ projects/Fabi, Salli/helpers.h
@@ -14,10 +14,10 @@ void stopAsuro(int pause) // While Schleife zum Blinken benötigt!
 	if (pause > 0)
 		msleep(pause);
 }
-/*float random() 
+/*float randomNumb() 
 {
 	int konst = 1;
-	int teiler[10] = {1,2,3,4,5,6,7,8,9,10};
-	randomNumber = konst / a;
-} */
+	float teiler[20] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10};
+	randomNumber = konst / teiler.at() 
+}*/
 
