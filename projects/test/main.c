417961c15e50d28275de6fbd3a0bbc2be4dcaaf3
3 <3>
2016-08-31 14:55:48 +0000

    Mir ist langweilig

--- projects/test/main.c
+++ projects/test/main.c
@@ -4,9 +4,9 @@
 #include <asuro/asuro.h>
 
 void main(void) {
-	int white[] = {0, 0};
-	int black[] = {0, 0};
+	uint16_t white[] = {0, 0};
 	uint16_t data[]  = {0, 0};
+	int turns = 0;
 
     Init();
     
@@ -14,42 +14,23 @@ void main(void) {
     MotorSpeed(80,80);
     
     char switches = PollSwitch();
-	int start_white = 0;
-	int start_black = 0;
 		
 	switches &= 0b00111111;
 		
 	LineData(data);
-	if(!start_white) {
-	if (switches = 32) {
-		LineData(data);
-		white[RIGHT] = data[RIGHT];
-		white[LEFT] = data[LEFT];
-		start_white = 1;
-	 }
-}
-	if(!start_black) {
-	if(switches = 16) {
-		LineData(data);
-		black[RIGHT] = data[RIGHT];
-		black[LEFT] = black[LEFT];
-		start_black = 1;
-	}
-}
-    if(switches = 8) {
-		MotorDir(FWD,FWD);
-		MotorSpeed(85,85);
-	}
-       
-    
-    while (1) {
-		LineData(data);
+	white[LEFT] = data[LEFT] - 200;
+	white[RIGHT] = data[RIGHT] - 200;
 	
-		if(data[RIGHT] == start_white) {
-		   MotorDir(FWD,FWD);
-		   MotorSpeed(85,85);
-		} else {
-		   GoTurn(0,90,80); 
-		}	
-	} 
+	while(1) {
+		LineData(data);
+		if((white[LEFT] > data[LEFT]) && (white[RIGHT] > data[RIGHT])) {
+			GoTurn(0,90,80);
+			turns++;
+			if(turns == 2) {
+				turns = 0;
+				GoTurn(0,180,80);
+			}
+		}
+		msleep(5);
+	}
 }
